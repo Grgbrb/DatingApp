cc4e5e2122b2b43262dfee26051adbc048505397
Grgbrb <grigoris.birbas@gmail.com>
2022-01-04 17:12:31 +0200

    Validation & Authorization Implemented

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,12 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using API.Entities;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    
+    public class UsersController : BasicApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,10 +19,19 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
+
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
 
         }
     }
