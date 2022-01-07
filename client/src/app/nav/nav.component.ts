8760b385919349379ae4e03a765a7514cb5738de
Grgbrb <grigoris.birbas@gmail.com>
2022-01-07 21:19:09 +0200

    Updated Angular & added toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class NavComponent implements OnInit {
   model: any = {}
   
 
-  constructor(public accountService: AccountService,private router: Router) { }
+  constructor(public accountService: AccountService,private router: Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -28,6 +29,7 @@ export class NavComponent implements OnInit {
       
     }, error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
